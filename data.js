f1fe988d4439227809ab0dff839e8e9f1f34747d
Elisabeth Finkel <32049677+esfinkel@users.noreply.github.com>
2019-08-17 18:32:27 -0500

    finkel superfamily data

--- data.js
+++ data.js
@@ -43,11 +43,11 @@ var FinkelPetersons = [
     { key: 8, n: "Maddie", s: "F", p1: 6, p2: 7, info: "Born 2002 in Chicago. Lives in Illinois" },
     { key: 9, n: "Jake", s: "M", p1: 6, p2: 7, info: "Born 2006 in Chicago. Lives in Illinois" },
     { key: "7a", n: "", s: "P", p1: 6, p2: 7, info: "Name: Luna. Species: Dog. Breed: Golden Doodle\nName: Midnight. Species: House Cat\nName: Tigris. Species: House Cat." },
-    ];
+];
 
-    // PATYK-FINKELS
+// PATYK-FINKELS
 
-    var PatykFinkels = [
+var PatykFinkels = [
 
     { key: 1, n: "Leo Patyk-Finkel", s: "M", p1: 2, p2: 3, info: "Born 2007, Gainesville FL. Lives in Vermont" },
     { key: 2, n: "Lynn", s: "F", p1: 16, p2: 19, ux: 3, info: "Born 1968, Elm Grove WI. Lives in Vermont, works at Dartmouth" },
@@ -55,12 +55,12 @@ var FinkelPetersons = [
     { key: 4, n: "Max", s: "M", p1: 2, p2: 3, info: "Born 2000, Palo Alto CA. Studies in Colorado" },
     { key: "3a", n: "Chloe", s: "P", p1: 2, p2: 3, info: "Species: Dog. Breed: Golden Doodle" },
 
-    ];
+];
 
-    // PATYKS 
+// PATYKS 
 
 
-    var Patyks = [
+var Patyks = [
 
     // Anders
 
@@ -70,22 +70,22 @@ var FinkelPetersons = [
     { key: 13, n: "Braden", s: "M", p1: 11, p2: 12, info: "Lives in  Wisconsin" },
     { key: 14, n: "Drew", s: "M", p1: 11, p2: 12, info: "Lives in Wisconsin" },
 
-    //Lisa(Patyk)
+    // Lisa(Patyk)
     { key: 15, n: "Lisa", s: "F", p1: 16, p2: 19, info: "Lives in Wisconsin" },
 
-    //Czarnecki Family
+    // Czarnecki Family
 
     {key: 16, n: "Gloria Czarnecki" , s: "F", p1: 17, p2: 18, ux: 19 },
     {key: 54, n: "Diane", s: "F", p1: 17, p2: 18 },
     {key: 57, n: "Lois", s: "F", p1: 17, p2: 18 },
     {key: 58, n: "Susan", s: "F", p1: 17, p2: 18, ux: "58a" },
-        {key: "58a", n: "Gary", s: "M", ux: 58 },
+    {key: "58a", n: "Gary", s: "M", ux: 58 },
         {key: "58b", n: "Shannon", s: "F", p1: 58, p2: "58a" },
     {key: "58c", n: "Joe", s: "M", p1: 58, p2: "58a" },
     {key: 17, n: "Stella Smerlinsky" , s: "F", ux: 18 },
     {key: 18, n: "Julian" , s: "M", ux: 17 },
 
-    //Patyk Family
+    // Patyk Family
 
     {key: 19, n: "Gary" , s: "M", p1: 20, p2: 21, ux: 16 },
     {key: 59, n: "Gregory", s: "M", p1: 20, p2: 21, ux: "59a" },
@@ -98,19 +98,19 @@ var FinkelPetersons = [
     {key: 55, n: "Joseph Kapzhak", s: "M", ux: 56 },
     {key: 56, n: "Angelina Hauke", s: "F", ux: 55 },
 
-    ];
+];
 
-    // FINKELS
+// FINKELS
 
-    var Finkels = [
+var Finkels = [
 
-    //Finkel (Richman)
+    // Finkel (Richman)
     {key: 22, n: "Frances \"Fran\"", s: "F", p1: -1, p2: -28, ux:23, info: "Lives in Pennsylvania. B Dec. 1940" },
     {key: 23, n: "Lawrence \"Larry\" Finkel", s: "M", p1: 38, p2: -33, ux: 22, info: "Born 1936" },
 
 
 
-    //Hunn
+    // Hunn
 
     { key: 30, n: "Ellen", s: "F", p1: 38, p2: -33, ux: 31 },
     { key: 31, n: "Ken Hunn", s: "M", ux: 30},
@@ -127,12 +127,14 @@ var FinkelPetersons = [
     { key: "37s", n: "Tia", s: "F", ux: 37 },
 
 
-    //Finkel
+    // Finkel
 
     { key: 38, n: "Samuel Finkel", s: "M", p1: 47, p2: 36 },
     { key: 39, n: "Cele Shulman", s: "F", ux: 38 },
     { key: 46, n: "Evelyn Kabatznick", s: "F", ux: 38 },
-    { key: 47, n: "Max Finkel", s: "M", ux: 36, info: "born March 15, 1883 in Kamyantes Podlis'kyy, Russia (now in Ukraine). Probably spoke Yiddish. Probably emigrated because of pogroms.\nWent to Glasgow, Scotland, then to Ellis Island in 1902. Lived in Brooklyn and Montreal, and probably philly. made mattress springs for \"Goodman Bros\"" },
+    { key: 47, n: "Max Finkel", s: "M", ux: 36, info: "born March 15, 1883 in \"in a town called Proskurov (since renamed Khmelnytskyi) _near_ the then larger town of Kamianets-Podilskyi,\" Russia (now in Ukraine) - Stuart Finkel.\nlisted as Mickel/Mikhel on the manifest of the Furnessia.\nProbably spoke Yiddish. Probably emigrated because of pogroms.\nWent to Glasgow, Scotland, then to Ellis Island in 1902. Lived in Brooklyn and Montreal, and probably philly.\nmade mattress springs for \"Goodman Bros\"" },
+
+
 
     { key: 36, n: "Yetta", s: "F", ux: 47, info: "Maiden name Horowitz. Probably spoke Yiddish. Probably from (\"shtetl near\") Vilna. Probably migrated because of pogroms.\nMoved to US  before 1904; lived in Brooklyn and Canada at some point." },
     { key: "47a", n: "Solomon?", s: "M", p1: 47, p2: 36 },
@@ -141,7 +143,7 @@ var FinkelPetersons = [
     { key: "47bb", n: "David Trusell", s: "M", ux: "47b", info: "owned a resort in Loon Lake, New York" },
     { key: "47bc", n: "Jack", s: "M", p1: "47b", p2: "47bb" },
 
-    //Brody
+    // Brody
 
     { key: 40, n: "Martha", s: "F", p1: 38, p2: 39, ux: 41 }, 
     { key: 41, n: "Arthur Brody", s: "M", ux: 40 },
@@ -149,11 +151,11 @@ var FinkelPetersons = [
     { key: 43, n: "Becky", s: "F", ux: 42 },
         { key: "42a", n: "Jill", s: "F", p1: 42, p2: 43 },
         { key: "42b", n: "Robin", s: "F", p1: 42, p2: 43 },
-    { key: 44, n: "Elissa", s: "F", p1: 40, p2: 41, ux: "45a" },
+    { key: 44, n: "Alyssa", s: "F", p1: 40, p2: 41, ux: "45a" },
     { key: 45, n: "Thomas", s: "M", p1: 44, p2: "45a" },
-        {key: "45a", n: "(Divorced)", s: "M", ux: 44 },
+    { key: "45a", n: "", s: "M", ux: 44 },
 
-    //Slotkin (sub)
+    // Slotkin (sub)
 
     { key: 48, n: "Evelyn", s: "F", p1: -34, p2: -35, ux: 49 },
     { key: 49, n: "Jack Rosen", s: "M", ux: 48 },
@@ -168,13 +170,13 @@ var FinkelPetersons = [
 
 
 
-    //Richman family
+    // Richman family
 
     { key: -1, n: "Samuel Richman", s: "M", p1: -2, p2: -7, ux: -28, info: "originally Reichman; legally changed name in 1929.\nhe was an \"upholder\"." },
-    { key: -2, n: "Louis", s: "M", p1: -3, p2: -4, ux: -7, info: "Louis Richman.\n\"born in 1887 in Akkerman, (Ackamann) Russia , came to America on a ship called \"Haverford\" from Bremen , Germany in 1906\" - Fran Finkel\nProbably spoke Yiddish; probably emigrated because of pogroms" },
+    { key: -2, n: "Louis Richman", s: "M", p1: -3, p2: -4, ux: -7, info: "Louis/Lewis; originally \"Abram Reichman.\"\n\"born in 1887 in Akkerman, (Ackamann) Russia , came to America on a ship called \"Haverford\" from Bremen , Germany in 1906\" - Fran Finkel\nProbably spoke Yiddish; probably emigrated because of pogroms.\nHad changed name by 1910" },
     { key: -7, n: "Sara(h)", s: "F", p1: -8, p2: -9, ux: -2, info: "Probably spoke Yiddish; probably emigrated 1900-1908 because of pogroms" },
-    { key: -3, n: "Isadore Rochman", s: "M", ux: -4, info: "Ize; \"born in 1845 in Ochumunn, Russia. He married in 1865 and died in 1890 and was a jeweler\" - fran" },
-    { key: -4, n: "Dora (Sabilinsky)", s: "M", p1: -5, p2: -6, ux: -3 },
+    { key: -3, n: "Isadore Reichman", s: "M", ux: -4, info: "Ize (Rochman?)\n\"born in 1845 in Ochumunn, Russia. He married in 1865 and died in 1890 and was a jeweler\" - fran" },
+    { key: -4, n: "Dora (Sablinsky)", s: "M", p1: -5, p2: -6, ux: -3, info: "(Sabilinsky?)\nhad entered US by 1910 census; believed to have been \"the only individual from that generation on either side of the Finkel-Richman family\" to come to North America - Stuart Finkel" },
     { key: -5, n: "Goldie Kriv", s: "F", ux: -6, info: "1830-1895; probably born Ochumunn, Russia" },
     { key: -6, n: "Mushie Sabilinsky", s: "M", ux: -5, info: "born 1829 - Ochumann, Russia" },
 
@@ -183,7 +185,7 @@ var FinkelPetersons = [
     { key: -10, n: "Goldie", s: "F", p1: -2, p2: -7, ux: -14 },
     { key: -14, n: "Herman Ellisand", s: "M", ux: -10 },
         { key: "-10a", n: "Harvey", s: "M", p1: -10, p2: -14 },
-        { key: "-10b", n: "Ivan", s: "M", p1: -10, p2: -14 },
+        { key: "-10b", n: "Irvin", s: "M", p1: -10, p2: -14 },
     { key: -11, n: "Jean", s: "F", p1: -2, p2: -7, ux: -17 },
         { key: -17, n: "Donald Schurr", s: "M", ux: -11 },
         { key: "-11a", n: "Arnold", s: "M", p1: -11, p2: -17 },
@@ -197,12 +199,12 @@ var FinkelPetersons = [
         { key: "-13a", n: "Andy", s: "M", p1: -13, p2: "-13c" },
         { key: "-13b", n: "Sharon", s: "F", p1: -13, p2: "-13c", info: "\"Cousin Sharon\"" },
 
-    { key: -18, n: "Israel", s: "M", p1: -3, p2: -4, ux: "-18s" },
+    { key: -18, n: "Israel", s: "M", p1: -3, p2: -4, ux: "-18s", info: "had entered US by 1910 census" },
     { key: "-18s", n: "Anna", s: "F", ux: -18 },
     { key: -19, n: "Izzie", s: "X", p1: -3, p2: -4, ux: "-19s" },
     { key: "-19s", n: "Cele", s: "X", ux: -19 },
     { key: "-19a", n: "Goldie, Herb, Victor", s: "X", p1: -19, p2: "-19s" },
-    { key: -20, n: "Charles", s: "M", p1: -3, p2: -4, ux: "-20s" },
+    { key: -20, n: "Charles", s: "M", p1: -3, p2: -4, ux: "-20s", info: "Charles Richman, originally Yankel Reichman. Came on the Haverford with (brother) Louis. Had changed name by 1910." },
     { key: "-20s", n: "Sara", s: "F", ux: -20 },
     { key: -21, n: "Minsa", s: "F", p1: -3, p2: -4, ux: "-21s" },
     { key: "-21s", n: "Bruce Potack", s: "M", ux: -21 },
@@ -254,10 +256,10 @@ var FinkelPetersons = [
 
     // Slotkin family
 
-    { key: "?2a", n: "?", s: "X", ux: "?2b", info: "Slotkin family immigrated from Vitebsk in 1906" },
-    { key: "?2b", n: "?", s: "X", ux: "?2a", info: "Slotkin family immigrated from Vitebsk in 1906" },
+    { key: "?2a", n: "", s: "X", ux: "?2b", info: "Zlotkin family immigrated from Vitebsk in 1906" },
+    { key: "?2b", n: "", s: "X", ux: "?2a", info: "Zlotkin family immigrated from Vitebsk in 1906" },
     { key: -33, n: "Edythe Slotkin", s: "F", p1: -34, p2: -35, ux: 38 },
-    { key: -34, n: "David Slotkin", s: "M", p1: "?2a", p2: "?2b", ux: -35, info: "Probably spoke Yiddish and emigrated from Vitebsk because of pogroms. Married and came to the US in 1906." },
+    { key: -34, n: "David Slotkin", s: "M", p1: "?2a", p2: "?2b", ux: -35, info: "Originally Zlotkin; had changed by 1920. Probably spoke Yiddish and emigrated from Vitebsk because of pogroms. Married and came to the US in 1906." },
     { key: -35, n: "Leah Pearlstein", s: "F", ux: -34, info: "\"probably born in Vitebsk. Probably spoke Yiddish and emigrated from Vitebsk because of pogroms. married and came here in 1906\" - fran" },
     { key: "-36a", n: "Max", s: "M", p1: "?2a", p2: "?2b", ux: "-36as" },
     { key: "-36as", n: "Frieda", s: "F", ux: "-36a" },
@@ -273,23 +275,22 @@ var FinkelPetersons = [
 
 
 
-    ];
+];
 
 
 
+// PETERSONS
 
-    // PETERSONS
-
-    var Petersons = [
+var Petersons = [
 
 
-    //Manthey
+    // Manthey
 
     { key: 24, n: "Emma Manthey", s: "F", p1: 25, p2: 26, info: "Lives in Illinois" },
     { key: 25, n: "Catherine", s: "F", p1: 28, p2: 29, ux: 26, info: "Lives in Illinois" },
     { key: 26, n: "Jim Manthey", s: "M", ux: 25, info: "Lives in Illinois" },
 
-    //Dave Jr. Peterson
+    // Dave Jr. Peterson
 
     { key: 27, n: "Dave Jr.", s: "M", p1: 28, p2: 29 },
 
@@ -341,7 +342,7 @@ var FinkelPetersons = [
 
 
     // Rosalind Peterson's older relatives
-        // Ingerskis
+	// Ingerskis
     { key: -51, n: "Rosalie (Ingerski)", s: "F", p1: -52, p2: -53, ux: -54, info: "Understood Polish (which her mother spoke).\nCatholic. After her marriage, her family \"turned on her\". She got along with Stella, Vic, and Florence.\nLost a set of twins." },
         { key: "-51t", n: "6 others", s: "X", p1: -52, p2: -53, info: "only 4 of whom survived" },
         { key: "-51u", n: "Anna", s: "F", p1: -52, p2: -53, info: "one of the oldest siblings" },
@@ -353,7 +354,7 @@ var FinkelPetersons = [
         { key: "-51x", n: "Victoria", s: "F", p1: -52, p2: -53, ux:"-51xs" },
         { key: "-51xs", n: "Lou Witry", s: "M", ux: "-51x", info: "Lou fought in WWI, got gassed by the Germans at one point.\nWas a Lt. in the police force" },
         { key: "-51xa", n: "Jean", s: "F", p1: "-51x", p2: "-51xs" },
-            { key: "-51xb", n: "Alan", s: "M", p1: "-51x", p2: "-51xs" },
+        { key: "-51xb", n: "Alan", s: "M", p1: "-51x", p2: "-51xs" },
         { key: "-51y", n: "Joe Jr.", s: "M", p1: -52, p2: -53, info: "Jerry, Bob, and a third boy (Joe?)" },
         { key: "-51z", n: "John", s: "M", p1: -52, p2: -53 },
     { key: -52, n: "Catherine (Rutkowski)", s: "F", ux: -53, info: "From Poland; \"someplace like Krakow or Warsaw.\" Spoke Polish and only a little English. (some of?) her children, inc. Rosalie, could understand Polish.\nLived in Minuck, IL" },
@@ -379,13 +380,9 @@ var FinkelPetersons = [
         { key: "-60s", n: "Beverly Jones", s: "F", ux: -60, info: "Her dad worked on the atom bomb plans.\nShe was a concert pianist, then a teacher. As a teacher, she'd go around the country setting up educational programs for schools" },
         { key: "-60a", n: "Jessica", s: "F", p1: -60, p2: "-60s", info: "Live in Missouri" },
         { key: "-60as", n: "", s: "M", ux: "-60a" }, // anon
-            { key: "-60ac", n: "3 kids", s: "X", p1: "-60a", p2: "-60as" },
+        { key: "-60ac", n: "3 kids", s: "X", p1: "-60a", p2: "-60as" },
         { key: "-60b", n: "Amy", s: "F", p1: -60, p2: "-60s", info: "Live in Missouri" },
         { key: "-60bs", n: "", s: "M", ux: "-60b" }, // anon
-            { key: "-60bc", n: "2 kids", s: "X", p1: "-60b", p2: "-60bs" },
-        
+        { key: "-60bc", n: "2 kids", s: "X", p1: "-60b", p2: "-60bs" },
 
-
-
-    ];
-
+];
